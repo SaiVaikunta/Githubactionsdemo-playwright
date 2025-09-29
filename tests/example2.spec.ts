7096000af57bfc2dd16195df96863839734a8c29
sai <saipavan216@gmail.com>
2025-09-29 16:30:29 +0530

    2909 updated from branch

--- tests/example2.spec.ts
+++ tests/example2.spec.ts
@@ -3,3 +3,7 @@ import { test, expect } from '@playwright/test';
 test('Welcome to the world of branches', async ({ page }) => {
  console.log("this is from world of branches")
 });
+
+test('Welcome to the world of branches', async ({ page }) => {
+ console.log("updated on 2909 from another branch")
+});
