54d4f29bf72098d56f114989e478e6f2a6838eee
sai <saipavan216@gmail.com>
2025-09-29 16:40:06 +0530

    commit after closing 2909

--- tests/example2.spec.ts
+++ tests/example2.spec.ts
@@ -1,9 +1,9 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, Page } from '@playwright/test';
 
-test('Welcome to the world of branches', async ({ page }) => {
+test('Welcome to the world of branches', async ({ page}) => {
  console.log("this is from world of branches")
 });
 
-test('Welcome to the world of branches', async ({ page }) => {
+test('Welcome to the world of branches', async ({ page }: { page: Page }) => {
  console.log("updated on 2909 from another branch")
 });
