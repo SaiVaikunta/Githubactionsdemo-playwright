d3dc252e0c747e41d2b269070cc6ed4718ac970f
sai <saipavan216@gmail.com>
2025-10-02 12:51:45 +0530

    starting playwright cucumber

--- tests/example2.spec.ts
+++ tests/example2.spec.ts
@@ -1,9 +1,6 @@
 import { test, expect, Page } from '@playwright/test';
 
 test('Welcome to the world of branches', async ({ page}) => {
- console.log("this is from world of branches")
+ console.log("starting playwright cucumber")
 });
 
-test('Welcome to the world of branches-corrected title', async ({ page }: { page: Page }) => {
- console.log("updated on 2909 from another branch")
-});
